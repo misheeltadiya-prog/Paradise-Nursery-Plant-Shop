abccdaa5dfea9515ea39a931cd8ea265e77a959e
misheeltadiya-prog <misheeltadiya@gmail.com>
2026-06-02 16:25:38 +0800

    Create AboutUs component for nursery information
    This component provides information about Paradise Nursery, including its mission and offerings.

---
+++ AboutUs.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Welcome to Paradise Nursery, your one-stop destination for beautiful 
+        and healthy plants. We are passionate about bringing nature closer to you.
+      </p>
+      <p>
+        Our mission is to make every home and office greener and more beautiful 
+        with carefully selected plants.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
